657eaae650c1632ceaad17347acc726e24220912
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-07-02 21:20:46 +0530

    Update database dependencies and settings

--- focustube/settings.py
+++ focustube/settings.py
@@ -82,11 +82,10 @@ WSGI_APPLICATION = 'focustube.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
